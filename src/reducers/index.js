9e12553559b37b0fdd266d23c2c2cdf3a63e4d76
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-02-11 00:53:17 +0530

    fix typos

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
-import { combinedReducers } from 'redux'
+import { combineReducers } from 'redux'
 import users from './users'
 import tweets from './tweets'
 import authedUser from './authedUser'
 
-export default combinedReducers({
+export default combineReducers({
   authedUser,
   users,
   tweets
