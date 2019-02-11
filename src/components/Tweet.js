c83a95e197ea583ebdcf380c94349ec29b1bde93
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-02-11 13:12:05 +0530

    add Tweet component and pass required data

---
+++ src/components/Tweet.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { formatTweet } from '../utils/helpers'
+
+class Tweet extends Component {
+  render () {
+    const { tweet } = this.props
+    if (tweet === null) {
+      return <p>This tweet doesn't exist.</p>
+    }
+    console.log(tweet)
+    return (
+      <div className='tweet'>
+        Tweet
+      </div>
+    )
+  }
+}
+
+function mapStateToProps ({ users, tweets, authedUser }, { id }) {
+  const tweet = tweets[id]
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null
+  return {
+    authedUser,
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null
+  }
+}
+
+export default connect(mapStateToProps)(Tweet)
