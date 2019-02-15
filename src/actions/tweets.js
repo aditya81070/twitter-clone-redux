c30e250d2721340622817dc44ffd7ceb9c406a91
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-02-15 12:05:20 +0530

    add actions for adding new tweet

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,6 +1,9 @@
-import { saveLikeToggle } from '../utils/api'
+import { saveLikeToggle , saveTweet } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
 export const TOGGLE_TWEET = 'TOGGLE_TWEETS'
+export const ADD_TWEET = 'ADD_TWEET'
 
 export function receiveTweets (tweets) {
   return {
@@ -18,6 +21,28 @@ function toggleTweet ({ id, authedUser, hasLiked }) {
   }
 }
 
+function addTweet (tweet) {
+  return {
+    type: ADD_TWEET,
+    tweet
+  }
+}
+
+export function handleAddTweet (text, replyingTo) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+
+    dispatch(showLoading())
+
+    return saveTweet({
+      text,
+      author: authedUser,
+      replyingTo
+    })
+      .then((tweet) => dispatch(addTweet(tweet)))
+      .then(() => dispatch(hideLoading()))
+  }
+}
 export function handleToggleTweet (info) {
   return (dispatch) => {
     dispatch(toggleTweet(info))
