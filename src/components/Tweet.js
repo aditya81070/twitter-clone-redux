33c335cb14d1644d9109ec6b814f25a027941c89
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-02-15 16:14:29 +0530

    add react router and complete all features

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
   handleLike = (e) => {
@@ -17,7 +18,7 @@ class Tweet extends Component {
 
   toParent = (e, id) => {
     e.preventDefault()
-    // TODO: redirect to the parent tweet
+    this.props.history.push(`/tweet/${id}`)
   }
 
   render () {
@@ -27,10 +28,10 @@ class Tweet extends Component {
     }
 
     const {
-      name, avatar, timestamp, text, hasLiked, likes, replies, parent
+      name, avatar, timestamp, text, hasLiked, likes, replies, id, parent
     } = tweet
     return (
-      <div className='tweet'>
+      <Link to={`/tweet/${id}`} className='tweet'>
         <img
           src={avatar}
           alt={`Avatar of ${name}`}
@@ -58,7 +59,7 @@ class Tweet extends Component {
             <span>{likes !== 0 && likes}</span>
           </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
@@ -74,4 +75,4 @@ function mapStateToProps ({ users, tweets, authedUser }, { id }) {
   }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
